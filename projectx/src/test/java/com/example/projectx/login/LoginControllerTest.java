c51efb2f272de8dac1f5695eee3d9668276b9019
up1 <somkiat.p@gmail.com>
2022-03-23 14:34:42 +0700

    Fixed controller testing

--- projectx/src/test/java/com/example/projectx/login/LoginControllerTest.java
+++ projectx/src/test/java/com/example/projectx/login/LoginControllerTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class LoginControllerTest {
@@ -14,8 +16,22 @@ class LoginControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @MockBean
+    private AdGateway adGateway;
+
+    @MockBean
+    private UserRepository userRepository;
+
     @Test
     void success_case_with_code_200() {
+        when(adGateway.isUserExisted("somkiat")).thenReturn(true);
+
+        User somkiat = new User();
+        somkiat.setId(1);
+        somkiat.setFirstName("somkiat");
+        somkiat.setLastName("pui");
+        when(userRepository.get("somkiat", "xxx")).thenReturn(somkiat);
+
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("somkiat");
         loginRequest.setPassword("xxx");
